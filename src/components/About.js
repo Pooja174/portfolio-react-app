5db4f8a7de69d2133dc8d58094126a2cd5b8410b
Poojitha <poojitha.j@tonal.com>
2024-03-21 17:04:53 +0530

    First commit

---
+++ src/components/About.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+function About() {
+  return (
+    <div
+      name="about"
+      className="w-full bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About
+          </p>
+        </div>
+        <p className="text-xl mt-20">
+          As a frontend developer with over 2 years of experience, I specialize
+          in creating user-friendly interfaces and implementing designs to
+          enhance the overall user experience. I have a strong understanding of
+          HTML, CSS, and JavaScript, along with experience with popular
+          frameworks like React.js. I am passionate about staying updated with
+          the latest trends and technologies in frontend development to
+          continuously improve my skills and deliver high-quality solutions.
+        </p>
+        <br></br>
+
+        <p className="text-xl">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
